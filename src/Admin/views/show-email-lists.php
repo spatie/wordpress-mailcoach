53ba99695709b57a206eabecd733c031403523e6
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2023-02-02 21:27:09 +0100

    copy spatie/laravel-mailcoach table.css file

--- src/Admin/views/show-email-lists.php
+++ src/Admin/views/show-email-lists.php
@@ -9,22 +9,26 @@ if (empty($lists->results())) {
 if (! empty($lists->results())) {
     $headers = ['Name', 'Active Subscribers', 'Created At'];
     ?>
-        <div>
-            <table class="email-lists-table">
-                <tr>
-                <?php foreach ($headers as $header) {
-                        echo "<th>{$header}</th>";
-                } ?>
-                </tr>
-                <?php foreach ($lists->results() as $list) {
-                    $created = wp_date(get_option( 'date_format' ), strtotime($list->attributes['created_at']));
+        <div class="card p-0 pb-24 md:pb-0 overflow-x-auto md:overflow-visible">
+            <table class="table-styled w-full">
+                <thead>
+                    <tr>
+                        <?php foreach ($headers as $header) {
+                            echo "<th>{$header}</th>";
+                        } ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($lists->results() as $list) {
+                        $created = wp_date(get_option( 'date_format' ), strtotime($list->attributes['created_at']));
 
-                    echo "<tr>";
-                    echo "<td>{$list->attributes['name']}</td>";
-                    echo "<td>{$list->attributes['active_subscribers_count']}</td>";
-                    echo "<td>{$created}</td>";
-                    echo "</tr>";
-                } ?>
+                        echo "<tr>";
+                        echo "<td>{$list->attributes['name']}</td>";
+                        echo "<td>{$list->attributes['active_subscribers_count']}</td>";
+                        echo "<td>{$created}</td>";
+                        echo "</tr>";
+                    } ?>
+                </tbody>
             </table>
         </div>
         <?php
