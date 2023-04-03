118e2af2e87cc83340751efd4b3c0c2ba5e8859a
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2023-04-03 09:34:07 +0200

    remove deadcode

--- src/functions.php
+++ src/functions.php
@@ -10,32 +10,3 @@ function anonymizeSensitiveDate(string $input): string
 
     return str_repeat('*', strlen($input) - 4) . substr($input, -4);
 }
-
-//add_action('template_redirect', 'createSubscriberFromShortCode');
-
-/**function createSubscriberFromShortCode(): void
-{
-    if (! isset($_POST['mailcoach_subscribe_submit']) || ! isset($_POST['mailcoach_subscribe_nonce'])) {
-        return;
-    }
-
-    if (! wp_verify_nonce($_POST['mailcoach_subscribe_nonce'], 'faire-don')) {
-        return;
-    }
-
-    if (isset($_POST['email_list_uuid'])) {
-        $emailListUuid = sanitize_text_field($_POST['email_list_uuid']);
-    }
-
-    $attributes = [];
-    foreach ($_POST as $key => $value) {
-        if (in_array($key, ['_wp_http_referer', 'mailcoach_subscribe_nonce', 'mailcoach_subscribe_submit', 'email_list_uuid'])) {
-            continue;
-        }
-
-        $attributes[$key] = sanitize_text_field($value);
-    }
-
-    // @todo use MailcoachApi class to subscribe
-}
-*/
