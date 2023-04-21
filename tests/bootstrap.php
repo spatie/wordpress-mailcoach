bd5edafaeb0ab68c9c2a2138cfad991e09c73c71
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2023-04-21 10:40:04 +0200

    composer changes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../wp/src/wp-includes/formatting.php';
+
+require_once __DIR__ . '/../wp/src/wp-includes/functions.php';
