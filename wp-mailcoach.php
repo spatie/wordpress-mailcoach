efa16a956155c5ed231048a857476c4252c8a05f
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2024-10-02 10:27:23 +0200

    tag 0.0.14

--- wp-mailcoach.php
+++ wp-mailcoach.php
@@ -9,7 +9,7 @@
  * that starts the plugin.
  *
  * @link              https://mailcoach.app
- * @since             0.0.13
+ * @since             0.0.14
  * @package           Mailcoach
  *
  * @wordpress-plugin
@@ -17,7 +17,7 @@
  * Plugin URI:        https://github.com/spatie/wordpress-mailcoach
  * Contributors:      nielsvanpach
  * Description:       Create forms for Mailcoach.
- * Version:           0.0.13
+ * Version:           0.0.14
  * Author:            Spatie
  * Author URI:        https://spatie.be
  * License:           MIT
